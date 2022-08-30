7a3d112420d4b8e5ff13685024d61ae96dd5f703
fujichi70 <nyq_o0@yahoo.co.jp>
2022-08-30 16:41:01 +0900

    やったことを保存機能実装

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,4 +4,5 @@
 //= link controllers/index.js
 //= link custom/time.js
 //= link custom/today.js
+//= link custom/addStartTime.js
 //= link_directory ../stylesheets .css
--- app/controllers/dones_controller.rb
+++ app/controllers/dones_controller.rb
@@ -4,17 +4,47 @@ class DonesController < ApplicationController
 	end
 	
 	def store
-		done = Done.new
 		
-		done.email       = current_user.email
-		done.done       = params[:done]
-		done.date      = params[:date]
-		done.start_time      = params[:start_time]
-		done.end_time      = params[:end_time]
-		done.created_at = Time.now.strftime('%Y-%m-%d')
-		done.save
-
-		redirect_to '/', notice: 'やったことを追加しました'
+		if params[:start_btn].present?
+			time = 
+			session[:done]       = params[:done]
+			session[:date]       = params[:date]
+			session[:start_time] = Time.now.strftime('%H:%M:%S')
+			session[:created_at] = Time.now.strftime('%Y-%m-%d')
+			redirect_to '/dones'
+		else
+			if session[:start_time].present?
+				done = Done.new
+				
+				if session[:done].present?
+					done.done = session[:done]
+				elsif params[:done].present?
+					done.done = params[:done]
+				else
+					redirect_to '/dones' , alert: '最初からやり直してください'
+				end
+				
+				if session[:date].present?
+					done.date = session[:date]
+				elsif params[:date].present?
+					done.date = params[:date]
+				else
+					redirect_to '/dones' , alert: '最初からやり直してください'
+				end
+				
+				done.email      = current_user.email
+				done.start_time = session[:start_time]
+				done.end_time   = Time.now.strftime('%H:%M:%S')
+				done.created_at = session[:created_at]
+				done.save
+				
+				session = ''
+				redirect_to '/dones', notice: 'やったことを追加しました'
+			else
+				redirect_to '/dones' , alert: '最初からやり直してください'
+			end	
+		end	
+		
 	end
 
 end
