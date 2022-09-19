e0f3a68a335a79250cb042da665d95fcbaa196d8
fujichi70 <nyq_o0@yahoo.co.jp>
2022-09-19 09:01:05 +0900

    js調整

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
 import "./custom/today.js";
 import "./custom/time.js";
+import "@hotwired/turbo-rails"
+import "controllers"
